0a113ed4b1fb25cc3bb4d5b612bac8f8ef26137a
126pikaqiu <35162936+126pikaqiu@users.noreply.github.com>
2018-06-14 22:29:43 +0800

    Add files via upload

--- userInfor.php
+++ userInfor.php
@@ -1,25 +1,25 @@
 <?php
+    session_start();
+    include_once "global.php";
     if($_SERVER["REQUEST_METHOD"] == 'GET'){
-        echo showInfor();
+        echo showInfor($db);
+    }else if($_SERVER["REQUEST_METHOD"] == 'POST'){
+        updateMyInfor($db);
+    }else{
+        exit("<strong>非法访问</strong>");
     }
-    
+
     //查询用户信息
-    function showInfor(){
-    
-        //连接到数据库
-        try{
-            $db = new PDO('mysql:host=localhost;dbname=users','root',"");
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch (PDOException $e){
-            print "Couldn't connect to the database;" . $e->getMessage();
-            exit();
-        }
-    
-        if(!isset($_GET['regName'])){
+    function showInfor($db){
+
+        if(isset($_GET["type"]) && isset($_GET["userID"])){
+            $p = $db->query("SELECT name, tel, email, address FROM users WHERE userID=". $_GET["userID"]);
+            return json_encode($p->fetch());
+        }   else  if(!isset($_GET['regName'])){
             print "<h1>非法访问</h1>";
             exit();
         }
-        $userName = $_GET['regName'];
+            $userName = $_GET['regName'];
         $p = $db->query("SELECT name, tel, email, address, balance FROM users");
     
         while($row = $p->fetch()){
@@ -29,7 +29,60 @@
     
         }
         return "false?用户不存在?";
-    
+    }
+
+    //修改信息
+    function updateMyInfor($db){
+        if(isset($_SESSION["myID"])){
+            if(isset($_POST["balance"])){
+                $balance = ", balance=".$_POST["balance"];
+            }else{
+                $balance = '';
+            }
+            if(isset($_POST["address"])){
+                $address = ", address=".'?';
+            }else{
+                $address = '';
+            }
+            if(isset($_POST["email"])){
+                $email = ", email="."?";
+            }else{
+                $email = '';
+            }
+            if(isset($_POST["tel"])){
+                $tel = ", tel=".$_POST["tel"];
+            }else{
+                $tel = '';
+            }
+            if(isset($_POST["regName"])){
+                $name = ", name="."?";
+            }else{
+                $name = '';
+            }
+            $id = $_SESSION["myID"];
+            $myid = "userID=" . $id;
+            try{
+                $sql = "UPDATE users SET userID=".$id .$name. $address . $balance. $email. $tel.
+                    ' WHERE '.$myid;
+                $result = $db->prepare($sql);
+                if($name && $address & $email){
+                    $aArray = array($_POST["regName"],$_POST["address"],$_POST["email"]);
+                }else if($name && $address){
+                    $aArray = array($_POST["regName"],$_POST["address"]);
+                }else if($name){
+                    $aArray = array($_POST["regName"]);
+                }else{
+                    $aArray = [];
+                }
+                $result = $result->execute($aArray);
+            }catch (PDOException $e){
+                print $e->getMessage();
+                exit();
+            }
+            echo "1";
+        }else{
+            echo "no found id";
+        }
     }
 ?>
 
--- workInfor.php
+++ workInfor.php
@@ -1,26 +1,52 @@
 <?php
+    session_start();
+    include "global.php";
+    //连接数据库
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=myproject','root',"");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch (PDOException $e){
+        print "Couldn't connect to the database;" . $e->getMessage();
+        exit();
+    }
     if($_SERVER["REQUEST_METHOD"] === 'GET'){
-        echo showInfor();
+        if(isset($_GET["order"]) && $_GET['order'] === "checkWork"){
+            try{
+                $p = $db->query("SELECT * FROM orders WHERE artworkID=" . $_GET["artworkID"]);
+                $row = $p->fetchAll();
+                if(count($row) > 0){
+                    echo "0";
+                }else{
+                    echo "1";
+                }
+                exit();
+            }catch (PDOException $e){
+                print "Couldn't connect to the database;" . $e->getMessage();
+                exit();
+            }
+        }
+        else if(isset($_GET["order"])){
+            showUploadWorks($db);
+            exit();
+        }else if(isset($_GET["key"]) && ($_GET["key"]==="getHotWorks" || $_GET["key"]==="getNewWorks" )){
+            showHotWorks($db);
+            exit();
+        }
+        echo showInfor($db);
+    }else if($_SERVER["REQUEST_METHOD"] === 'POST'){
+        if(isset($_POST["order"]) && isset($_POST["artworkID"])){
+            handleOrder($db);
+        }
     }else{
         exit("<strong>非法访问</strong>");
     }
 
-    //查询用户信息
-    function showInfor(){
-
-        //连接到数据库
-        try{
-            $db = new PDO('mysql:host=localhost;dbname=artworks','root',"");
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch (PDOException $e){
-            print "Couldn't connect to the database;" . $e->getMessage();
-            exit();
-        }
+    //查询作品信息
+    function showInfor($db){
         if(!isset($_GET['key'])){
             exit("<strong>非法访问</strong>");
         }
         $key = $_GET['key'];
-
         $p = $db->query("SELECT  `artworkID`, `artist`, `imageFileName`, `title`, `description`,
  `yearOfWork`, `genre`, `width`, `height`, `price`, `view` FROM artworks ");
 
@@ -46,5 +72,42 @@
             return "false|";
         }
 
+    }
+
+    //我上传的艺术品信息
+    function showUploadWorks($db){
+        if(isset($_SESSION["myID"])){
+            $p = $db->query("SELECT  `artworkID`, `title`, `timeReleased` FROM artworks WHERE ownerID=" . $_SESSION["myID"]);
+            $works=[];
+            $i = 0;
+            while($i < 5 && $row = $p->fetch()){
+               $works[] = $row;
+               $i++;
+            }
+            echo json_encode($works);
+        }
+    }
+
+    //热门艺术品信息
+    function showHotWorks($db){
+        if($_GET["key"] === "getNewWorks"){
+            $p = $db->query("SELECT * FROM artworks Order By `artworkID` desc limit 6");
+            $works = $p->fetchAll();
+            echo json_encode($works);
+        }else if($_GET["key"] === "getHotWorks"){
+            $p = $db->query("SELECT * FROM artworks Order By `view` desc limit 6");
+            $works = $p->fetchAll();
+            echo json_encode(utf8ize($works));
+        }
+    }
+
+    //删除艺术品
+    function handleOrder($db){
+        $p = $db->exec("DELETE FROM artworks WHERE artworkID=" . $_POST["artworkID"]);
+        if($p){
+            echo "1";
+        }else{
+            echo "0";
+        }
     }
 ?>
